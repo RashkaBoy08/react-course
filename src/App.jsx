0050732a336b3724e1da504df8eeef5b4aee2e33
Student <student@example.com>
2026-08-15 11:32:57 +0300

    Initial commit: React course project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Greeting, WelcomeMessage } from "./Greeting";
+
+function App() {
+  return (
+    <>
+      <Greeting />;
+      <WelcomeMessage />;
+    </>
+  );
+}
+
+export default App;
