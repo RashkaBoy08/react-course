43d5df6ad81933f27b94f8f02fd28f38b66d208a
Student <student@example.com>
2026-09-04 20:54:50 +0300

    Add color change controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-import { MouseTracker } from "./MouseTracker";
+import { useState } from "react";
 function App() {
+  const [color, setColor] = useState();
+
+  const switchToRed = () => {
+    setColor("Red");
+  };
+
+  //green color
+  const switchToGreen = () => {
+    setColor("green");
+  };
+
+  const switchToBlue = () => {
+    setColor("blue");
+  };
+
+  const randomColor = () => {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    setColor(`rgb(${red}, ${green}, ${blue})`);
+  };
+
   //markup
-  return <MouseTracker />;
+  return (
+    <div>
+      <div
+        style={{
+          backgroundColor: color,
+          padding: "30px",
+          margin: "20px",
+          borderRadius: "10px",
+          color: "#fff",
+          textAlign: "center",
+          fontSize: "24px",
+          fontWeight: "600",
+        }}
+      >
+        {color}
+      </div>
+      <h1>Color change</h1>
+      <button onClick={switchToRed}>Red background</button>
+      <button onClick={switchToGreen}>Green background</button>
+      <button onClick={switchToBlue}>Blue background</button>
+      <button onClick={randomColor}>Random background</button>
+    </div>
+  );
 }
 
 export default App;
